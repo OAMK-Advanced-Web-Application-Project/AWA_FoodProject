e4f7fb41136f7c3e8374f11d7712f802d4818814
sarafarahabadi <sarafarahabadi@gmail.com>
2021-11-04 16:01:22 +0200

    server/server.js added

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '1216',
+    database: 'food'
+});
+
+/* //user signup 
+app.post('/signup', function (req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const emailaddress = req.body.emailaddress;
+    const password = req.body.password;
+    const address = req.body.address;
+
+    db.query('INSERT INTO user (fistname, lastname, emailaddress, password, address) values(?,?,?,?,?)', 
+        [firstName, lastName, emailaddress, password, address],
+        (err, result) => {
+            if (err) {
+                console.error(err);
+            } else{
+                res.send("values Inserted");
+            }
+        }
+    );
+}); */
+
+
+app.listen(3001, () => {
+    console.log("Your server is running on port 3001");
+});
+
