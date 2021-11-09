b94ee5268de8fc2c7775739d61c7b2638f2ccfdf
AkiP4 <67232776+AkiP4@users.noreply.github.com>
2021-11-09 09:05:21 +0200

    Separated two different navbar versions
    Separated two different navbar versions and made the changes needed to run the application

--- client/src/components/NavbarNoSearch.js
+++ client/src/components/NavbarNoSearch.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import "./Navbar.css";
 
-class Navbar extends React.Component {
+class NavbarNoSearch extends React.Component {
   render() {
     return (
       <div class="NavbarBase">
         <div class="NavbarContent">
           <img class="MainLogo" src="images/jolt_logo.png" alt="Logo" />
-          <input class="SearchBox" type="text" placeholder="Search"/>
         </div>
       </div>
     );
   }
 }
 
-export default Navbar
+export default NavbarNoSearch
 
 
   
---
+++ client/src/components/NavbarWithSearch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Navbar.css";
+
+class NavbarWithSearch extends React.Component {
+  render() {
+    return (
+      <div class="NavbarBase">
+        <div class="NavbarContent">
+          <img class="MainLogo" src="images/jolt_logo.png" alt="Logo" />
+          <input class="SearchBox" type="text" placeholder="Search"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NavbarWithSearch
+
+
+  
