d52a71ad62b6e10891206c7246d288dd62a7295a
AkiP4 <67232776+AkiP4@users.noreply.github.com>
2021-11-10 13:39:45 +0200

    Added and created the routes
    Connected the landing page to login/sign up for both the restaurant and the customer portals.

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,27 @@
 /*import './App.css';*/
 import NavbarNoSearch from "./components/NavbarNoSearch.js"
 import React from 'react';
-/*import UserLogin from './components/userLogin';*/
-/*import UserSignup from './components/userSignup';*/
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage.js";
+import UserLogin from './components/userLogin.js';
+import RestaurantLogin from './components/restaurantLogin.js';
+import UserSignup from './components/userSignup';
+import RestaurantSignup from './components/restaurantSignup';
 
 function App() {
   return (
     <div>
-
-      <NavbarNoSearch/>
-      <LandingPage />
-{/*       <UserLogin /> */}
+      <Router>
+          <NavbarNoSearch/>
+          
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/userlogin" element={<UserLogin/>} />
+            <Route path="/restaurantlogin" element={<RestaurantLogin/>} />
+            <Route path="/usersignup" element={<UserSignup/>} />
+            <Route path="/restaurantsignup" element={<RestaurantSignup/>} />
+          </Routes>
+      </Router>
 
     </div>
   );
