55b0e23cc83190e5f7a58d26c885c2f65bace806
Aleksi <79052109+Haxeli@users.noreply.github.com>
2021-11-11 15:35:01 +0200

    Update App.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+/*import './App.css';*/
+import NavbarNoSearch from "./components/NavbarNoSearch.js"
+import React from 'react';
+/*import UserLogin from './components/userLogin';*/
+import UserSignup from './components/userSignup';
+
+function App() {
+  return (
+    <div>
+
+      <NavbarNoSearch/>
+      <UserSignup />
+{/*       <UserLogin /> */}
+
+    </div>
+  );
+}
+
+export default App;
