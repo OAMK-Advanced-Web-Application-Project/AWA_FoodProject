5ae06b798694ab5d72035af7cb81478528dcce78
sarafarahabadi <sarafarahabadi@gmail.com>
2021-11-15 13:13:44 +0200

    edit to resolve issue

--- client/src/components/searchResult.js
+++ client/src/components/searchResult.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './mainpage.css'
-import { Link } from "react-router-dom";
-
+import styles from './searchView.module.css'
 
 export default function searchResult(props) {
     return (
         <div className="restaurant">
+        <div className={styles.restaurant}>
             <div><img src={`./images/${props.image}`}/></div>
             <div><b>{ props.name }</b></div>
             <div>{ props.type }</div>
             <div>{ props.price }</div>
-            <Link to="/restaurantPage">To Restaurant</Link>
+        </div>,
+        <div className={styles.restaurant}>
+            <div><img src={`./images/${props.image}`} alt={"restaurant"}/></div>
+            <div className={styles.name}> { props.name } </div>
+            <div className={styles.type}> { props.type } </div>
+            <div className={styles.price}> { props.price } </div>
         </div>
     )
 }
