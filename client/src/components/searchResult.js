2853a0dcef61e8e983b6261bfe29a84706428f17
sarafarahabadi <sarafarahabadi@gmail.com>
2021-11-15 13:10:44 +0200

    resolving issue

--- client/src/components/searchResult.js
+++ client/src/components/searchResult.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 export default function searchResult(props) {
     return (
-        <div className="restaurant">
             <div><img src={`./images/${props.image}`}/></div>
             <div><b>{ props.name }</b></div>
             <div>{ props.type }</div>
