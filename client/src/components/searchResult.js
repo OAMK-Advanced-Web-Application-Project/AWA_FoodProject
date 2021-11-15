83569a75a21e8a1bc4aecac1cbf55945c1ecee36
sarafarahabadi <sarafarahabadi@gmail.com>
2021-11-15 13:08:39 +0200

    resolving issue

--- client/src/components/searchResult.js
+++ client/src/components/searchResult.js
@@ -4,6 +4,12 @@ import styles from './searchView.module.css'
 export default function searchResult(props) {
     return (
         <div className={styles.restaurant}>
+            <div><img src={`./images/${props.image}`}/></div>
+            <div><b>{ props.name }</b></div>
+            <div>{ props.type }</div>
+            <div>{ props.price }</div>
+        </div>,
+        <div className={styles.restaurant}>
             <div><img src={`./images/${props.image}`} alt={"restaurant"}/></div>
             <div className={styles.name}> { props.name } </div>
             <div className={styles.type}> { props.type } </div>
