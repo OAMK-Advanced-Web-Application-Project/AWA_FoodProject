82d417f3e531480e516ef1c7c82be6456ae31d40
sarafarahabadi <sarafarahabadi@gmail.com>
2021-11-17 16:01:22 +0200

    Landing page CSS

--- client/src/components/landingPage/LandingPage.js
+++ client/src/components/landingPage/LandingPage.js
@@ -1,24 +1,37 @@
 import React from "react";
-import "./LandingPage.css";
 import { Link } from "react-router-dom";
-
+import styles from "./LandingPage.module.css";
 
 class LandingPage extends React.Component {
-    render() {
-      return (
-        <div className="landingContainer">
-            <div className="containerContent">
-                    <p>Exquisite flavors.</p>
-                    <p>Simply delivered.</p>
-            <div className="loginButtons">
-                <Link to="/restaurantlogin" className="buttonRestaurant">Log in as a bistro</Link>
-                <Link to ="/userlogin" className="buttonCustomer">Log in as a customer</Link>
+  render() {
+    return (
+      <div class={styles.background}>
+        <div class={styles.bothLandings}>
+          <div class={styles.landingContainer}>
+            <div class={styles.containerContent}>
+              <p>Expand your business and offer your delicious meals</p>
+              <div class={styles.loginButtons}>
+                <Link to="/restaurantlogin">
+                  Log in as a bistro
+                </Link>
+              </div>
             </div>
-
+          </div>
+          <div class={styles.landingContainer}>
+            <div class={styles.containerContent}>
+              <p>Hungry?</p>
+              <p>Your order is coming!</p>
+              <div class={styles.loginButtons}>
+                <Link to="/userlogin">
+                  Log in as a customer
+                </Link>
+              </div>
             </div>
+          </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default LandingPage
+}
+
+export default LandingPage;
