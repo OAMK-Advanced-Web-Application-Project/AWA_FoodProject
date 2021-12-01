c3bce877474c3620b5f5e723c59ae3c5a80010a8
sarafarahabadi <sarafarahabadi@gmail.com>
2021-12-01 16:52:51 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3002"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
     credentials: true,
   })
