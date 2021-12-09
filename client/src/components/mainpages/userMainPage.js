e3a5767e5232c5bfc54866df80a47fa1a19707ce
Aleksi <79052109+Haxeli@users.noreply.github.com>
2021-12-09 19:25:55 +0200

    new usermainpage

--- client/src/components/mainpages/userMainPage.js
+++ client/src/components/mainpages/userMainPage.js
@@ -5,18 +5,14 @@ import Constants from "../Constants.json";
 
 export default function SearchResult() {
   const [restaurantShow, setRestaurantShow] = useState();
+  const [searchTerm, setsearchTerm] = useState("");
 
   useEffect(() => {
-    const Idata = async (event) => {
-      event.preventDefault();
-      const result = await Axios.get(
-        Constants.API_ADDRESS + "/fetchData/restaurants",
-        {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      );
+    Axios.get(Constants.API_ADDRESS + "/fetchData/restaurants", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    }).then((result) => {
       const restaurantArray = result.data;
       const restaurantShow = restaurantArray.map((elem) => ({
         id: elem.idrestaurant,
@@ -24,25 +20,35 @@ export default function SearchResult() {
         type: elem.type,
         pricelevel: elem.pricelevel,
       }));
-      console.log(restaurantShow);
       setRestaurantShow(restaurantShow);
-    };
-    window.addEventListener("load", Idata);
-  })
+    })
+  },[])
   
+
   return (
-    <div className={styles.restaurant}>
-      {" "}
-      <div>
-        
-        {restaurantShow && restaurantShow.map((show, id) => (
-          <div key={id}>
-            <h3>{show.restaurantname}</h3>
-            <h4>€{show.type}</h4>
-            <h4>€{show.pricelevel}</h4>
+    <div>
+      <input
+          class="SearchBox"
+          className={styles.SearchBox}
+          type="text"
+          onChange={(event) => {
+            setsearchTerm(event.target.value);
+          }}
+          placeholder="Search for restaurants..."
+        />
+      {restaurantShow.filter((show) => {
+        if(searchTerm == "") {
+          return show
+        } else if (show.restaurantname.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return show
+        }
+        }).map((show, id) => (
+          <div className={styles.restaurant} key={id}>
+            <div className={styles.name}>{show.restaurantname}</div>
+            <div className={styles.type}>{show.type}</div>
+            <div className={styles.pricelevel}>{show.pricelevel}</div>
           </div>
         ))}
       </div>
-    </div>
   );
 }
