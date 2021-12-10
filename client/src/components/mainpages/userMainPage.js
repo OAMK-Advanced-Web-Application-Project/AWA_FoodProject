94a5c6de101d3f85b62e5d3c844752a1dd9490b8
Aleksi <79052109+Haxeli@users.noreply.github.com>
2021-12-10 11:22:52 +0200

    Search works

--- client/src/components/mainpages/userMainPage.js
+++ client/src/components/mainpages/userMainPage.js
@@ -1,29 +1,30 @@
 import Axios from "axios";
-import React, { useState, useEffect } from "react";
-import styles from "./searchView.module.css";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./userMainPage.module.css";
 import Constants from "../Constants.json";
 
 export default function SearchResult() {
   const [restaurantShow, setRestaurantShow] = useState();
   const [searchTerm, setsearchTerm] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     Axios.get(Constants.API_ADDRESS + "/fetchData/restaurants", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
-      }
+      },
     }).then((result) => {
       const restaurantArray = result.data;
       const restaurantShow = restaurantArray.map((elem) => ({
-        id: elem.idrestaurant,
+        idrestaurant: elem.idrestaurant,
         restaurantname: elem.restaurantname,
         type: elem.type,
         pricelevel: elem.pricelevel,
       }));
       setRestaurantShow(restaurantShow);
-    })
-  },[])
-  
+    });
+  }, []);
 
   return (
     <div>
@@ -36,21 +37,57 @@ export default function SearchResult() {
           }}
           placeholder="Search for restaurants..."
         />
-      {restaurantShow.filter((show) => {
-        if(searchTerm == "") {
-          return show
-        } else if (show.restaurantname.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return show
-        }
-      }).map((show, id) => {
-          return (
-          <div className={styles.restaurant} key={id}>
-            <div className={styles.name}>{show.restaurantname}</div>
-            <div className={styles.type}>{show.type}</div>
-            <div className={styles.pricelevel}>{show.pricelevel}</div>
-          </div>
-        )})
-      }
+      <div>
+        {restaurantShow && restaurantShow.filter((show) => {
+          if (searchTerm == "") {
+            return show
+          }
+          else if (show.restaurantname.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return show
+          }
+        })
+        .map((show, idrestaurant) => {
+            return (
+              <div
+                className={styles.restaurant}
+                key={idrestaurant}
+                onClick={() => {
+                  navigate(`/restaurantmenu/${show.idrestaurant}`);
+                }}>
+                  <div className={styles.name}>{show.restaurantname}</div>
+                  <div className={styles.type}>{show.type}</div>
+                  <div className={styles.price}>{show.pricelevel}</div>
+              </div>
+            );
+          })}
       </div>
+    </div>
   );
 }
+
+// <div>
+    //   <input
+    //       class="SearchBox"
+    //       className={styles.SearchBox}
+    //       type="text"
+    //       onChange={(event) => {
+    //         setsearchTerm(event.target.value);
+    //       }}
+    //       placeholder="Search for restaurants..."
+    //     />
+    //   {restaurantShow.filter((show) => {
+    //     if(searchTerm == "") {
+    //       return show
+    //     } else if (show.restaurantname.toLowerCase().includes(searchTerm.toLowerCase())) {
+    //       return show
+    //     }
+    //   }).map((show, id) => {
+    //       return (
+    //       <div className={styles.restaurant} key={id}>
+    //         <div className={styles.name}>{show.restaurantname}</div>
+    //         <div className={styles.type}>{show.type}</div>
+    //         <div className={styles.pricelevel}>{show.pricelevel}</div>
+    //       </div>
+    //     )})
+    //   }
+    //   </div>
